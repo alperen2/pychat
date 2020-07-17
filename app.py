97e6362a031c9a1420ebcdd74a9335762f19370c
alperen2 <aalperendurmuss@gmail.com>
2020-07-17 23:36:12 +0300

    Arayüz geliştirildi, session kontrolü sağlandı

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask_mysqldb import MySQL
 from flask import Flask, render_template, request,flash, redirect, url_for, session, request, Response
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from functools import wraps
-
+from flask_session import Session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '*\x9c\xd8J\x81\x11x\xf0c\xfa\xfc\xde:\xef\x03x'
@@ -11,17 +11,19 @@ app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "root"
 app.config["MYSQL_DB"] = "wp_db"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
-socketio = SocketIO(app)
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+
+socketio = SocketIO(app, manage_session = False)
 mysql = MySQL(app)
-
-
 
 # kullanıcı giriş decarator
 def check_login(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if "username" not in session:
-            return redirect(url_for('login'))
+            return redirect(url_for('index'))
         return f(*args, **kwargs)
     return decorated_function
 
@@ -30,15 +32,29 @@ def check_login(f):
 def index():
     return render_template("index.html")
 
-
+@socketio.on('session_control')
+def session_control():
+    if 'username' in session:
+        emit('session_control')
+    
+@check_login
 @socketio.on('join')
-def handle_join(username):  
-    session['username'] = username
-    emit('joined', "{} joined".format(username), broadcast=True, include_self=False)
-   
+def handle_join(data):
+    username = data['username']
+    password = data['password']
+    cursor = mysql.connection.cursor()
+    query = cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
+    if query == 1:
+        user = cursor.fetchone()
+        session['username'] = username
+        emit('joined')
+        emit('joined_message', "{} joined".format(username), broadcast=True, include_self=False)
+    else:
+        emit('loginFailed', "{} kullanıcısı bulunamadı ya da parola yanlış".format(username))
+
+@check_login
 @socketio.on('sendMessage')
 def handle_message(data):
-    print(data)
     message = data['message']
     room = data['room_name']
     emit('my response', ("{}".format(message)), broadcast=True)
